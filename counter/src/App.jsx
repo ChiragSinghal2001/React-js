772ec4757b3d4f3ce5d7a6fb1590ef2c24b40aca
ChiragSinghal2001 <chirag.singhal5088@gmail.com>
2024-01-08 15:34:19 +0530

    Initial commit

---
+++ counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    let [counter, setCounter] = useState(17)
+  // let counter=5;
+
+  function addValue(){
+    counter++;
+    setCounter(counter)
+    console.log(counter);
+  }
+  function removeValue(){
+    if(counter>0){
+      counter--;
+    setCounter(counter)
+    } 
+    console.log(counter);
+  }
+  return (
+    <>
+      <h1>Akh dikhata hai </h1>
+      <h3>Counter Value : {counter}</h3>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}  disabled={counter === 0}>Remove value Value</button>
+      <p>Dekhte hai yaha value update hogi ke nhi {counter}</p>
+
+    </>
+  )
+}
+
+export default App
